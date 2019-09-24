926c8a351aebe17a31e3a3fcc442e2ba601134c1
Adam Gibson <gibsoncadam@gmail.com>
2019-09-24 14:01:41 -0700

    here is the attempt at recursive method

--- public/src/app/path/path.component.ts
+++ public/src/app/path/path.component.ts
@@ -12,6 +12,31 @@ import { HttpService } from './../http.service';
 export class PathComponent implements OnInit {
   theDivs: any;
   mousedown: boolean = false;
+  currentNumber: Number = 1;
+  // theArray = [
+  //   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  //   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  //   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  //   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  //   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  //   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  //   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  //   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  //   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  //   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  // ]
+  theArray = [
+    [{ value: 0, hasVisited: "false", count: 0 }, { value: 0, hasVisited: "false", count: 0 }, { value: 0, hasVisited: "false", count: 0 }, { value: 0, hasVisited: "false", count: 0 }, { value: 0, hasVisited: "false", count: 0 }, { value: 0, hasVisited: "false", count: 0 }, { value: 0, hasVisited: "false", count: 0 }, { value: 0, hasVisited: "false", count: 0 }, { value: 0, hasVisited: "false", count: 0 }, { value: 0, hasVisited: "false", count: 0 }],
+    [{ value: 0, hasVisited: "false", count: 0 }, { value: 0, hasVisited: "false", count: 0 }, { value: 0, hasVisited: "false", count: 0 }, { value: 0, hasVisited: "false", count: 0 }, { value: 0, hasVisited: "false", count: 0 }, { value: 0, hasVisited: "false", count: 0 }, { value: 0, hasVisited: "false", count: 0 }, { value: 0, hasVisited: "false", count: 0 }, { value: 0, hasVisited: "false", count: 0 }, { value: 0, hasVisited: "false", count: 0 }],
+    [{ value: 0, hasVisited: "false", count: 0 }, { value: 0, hasVisited: "false", count: 0 }, { value: 0, hasVisited: "false", count: 0 }, { value: 0, hasVisited: "false", count: 0 }, { value: 0, hasVisited: "false", count: 0 }, { value: 0, hasVisited: "false", count: 0 }, { value: 0, hasVisited: "false", count: 0 }, { value: 0, hasVisited: "false", count: 0 }, { value: 0, hasVisited: "false", count: 0 }, { value: 0, hasVisited: "false", count: 0 }],
+    [{ value: 0, hasVisited: "false", count: 0 }, { value: 0, hasVisited: "false", count: 0 }, { value: 0, hasVisited: "false", count: 0 }, { value: 0, hasVisited: "false", count: 0 }, { value: 0, hasVisited: "false", count: 0 }, { value: 0, hasVisited: "false", count: 0 }, { value: 0, hasVisited: "false", count: 0 }, { value: 0, hasVisited: "false", count: 0 }, { value: 0, hasVisited: "false", count: 0 }, { value: 0, hasVisited: "false", count: 0 }],
+    [{ value: 0, hasVisited: "false", count: 0 }, { value: 0, hasVisited: "false", count: 0 }, { value: 0, hasVisited: "false", count: 0 }, { value: 0, hasVisited: "false", count: 0 }, { value: 0, hasVisited: "false", count: 0 }, { value: 0, hasVisited: "false", count: 0 }, { value: 0, hasVisited: "false", count: 0 }, { value: 0, hasVisited: "false", count: 0 }, { value: 0, hasVisited: "false", count: 0 }, { value: 0, hasVisited: "false", count: 0 }],
+    [{ value: 0, hasVisited: "false", count: 0 }, { value: 0, hasVisited: "false", count: 0 }, { value: 0, hasVisited: "false", count: 0 }, { value: 0, hasVisited: "false", count: 0 }, { value: 0, hasVisited: "false", count: 0 }, { value: 0, hasVisited: "false", count: 0 }, { value: 0, hasVisited: "false", count: 0 }, { value: 0, hasVisited: "false", count: 0 }, { value: 0, hasVisited: "false", count: 0 }, { value: 0, hasVisited: "false", count: 0 }],
+    [{ value: 0, hasVisited: "false", count: 0 }, { value: 0, hasVisited: "false", count: 0 }, { value: 0, hasVisited: "false", count: 0 }, { value: 0, hasVisited: "false", count: 0 }, { value: 0, hasVisited: "false", count: 0 }, { value: 0, hasVisited: "false", count: 0 }, { value: 0, hasVisited: "false", count: 0 }, { value: 0, hasVisited: "false", count: 0 }, { value: 0, hasVisited: "false", count: 0 }, { value: 0, hasVisited: "false", count: 0 }],
+    [{ value: 0, hasVisited: "false", count: 0 }, { value: 0, hasVisited: "false", count: 0 }, { value: 0, hasVisited: "false", count: 0 }, { value: 0, hasVisited: "false", count: 0 }, { value: 0, hasVisited: "false", count: 0 }, { value: 0, hasVisited: "false", count: 0 }, { value: 0, hasVisited: "false", count: 0 }, { value: 0, hasVisited: "false", count: 0 }, { value: 0, hasVisited: "false", count: 0 }, { value: 0, hasVisited: "false", count: 0 }],
+    [{ value: 0, hasVisited: "false", count: 0 }, { value: 0, hasVisited: "false", count: 0 }, { value: 0, hasVisited: "false", count: 0 }, { value: 0, hasVisited: "false", count: 0 }, { value: 0, hasVisited: "false", count: 0 }, { value: 0, hasVisited: "false", count: 0 }, { value: 0, hasVisited: "false", count: 0 }, { value: 0, hasVisited: "false", count: 0 }, { value: 0, hasVisited: "false", count: 0 }, { value: 0, hasVisited: "false", count: 0 }],
+    [{ value: 0, hasVisited: "false", count: 0 }, { value: 0, hasVisited: "false", count: 0 }, { value: 0, hasVisited: "false", count: 0 }, { value: 0, hasVisited: "false", count: 0 }, { value: 0, hasVisited: "false", count: 0 }, { value: 0, hasVisited: "false", count: 0 }, { value: 0, hasVisited: "false", count: 0 }, { value: 0, hasVisited: "false", count: 0 }, { value: 0, hasVisited: "false", count: 0 }, { value: 0, hasVisited: "false", count: 0 }]
+  ]
 
   constructor(
     private _route: ActivatedRoute,
@@ -21,25 +46,30 @@ export class PathComponent implements OnInit {
 
   ngOnInit() {
     this.displayGrid();
+    this.SearchAround(this.theArray, 6, 3, 0)
   }
   // setStart(val) {
   //   (document.querySelector(`#${val}`) as HTMLElement).style.background = "black";
   // }
   displayGrid() {
     var output1 = [];
-    for (var x = 0; x < 10; x++) {
+    for (var x = 0; x < this.theArray.length; x++) {
       var output2 = [];
-      // output2.push(`row`)
-      for (var i = 0; i < 10; i++) {
+      for (var i = 0; i < this.theArray[x].length; i++) {
         output2.push(`babyBox${x}${i}`)
       }
       output1.push(output2);
     }
     console.log(output1[6][3])
+    output1[4][8] = 'endBox48'
+    // this.theArray[4][8] = 10;
+    this.theArray[4][8].value = 10;
     output1[6][3] = 'startBox63';
+    // this.theArray[6][3] = 5;
+    this.theArray[6][3].value = 5;
     console.log(output1[6][3])
+    console.log(output1);
     this.theDivs = output1;
-    // this.setStart('startBox63');
   }
 
   MouseDown(divId) {
@@ -50,18 +80,87 @@ export class PathComponent implements OnInit {
   }
   clickNhover(divId) {
     if (this.mousedown) {
-      if (divId != 'startBox63') {
+      if (divId != 'startBox63' && divId != 'endBox48') {
+        var x = divId[divId.length - 2]
+        var y = divId[divId.length - 1]
+        // this.theArray[x][y] = 2;
+        this.theArray[x][y].value = 2;
+        console.log(this.theArray);
         if ((document.querySelector(`#${divId}`) as HTMLElement).style.background == "red") {
           (document.querySelector(`#${divId}`) as HTMLElement).style.background = "white";
         }
         else {
           (document.querySelector(`#${divId}`) as HTMLElement).style.background = "red";
+          console.log((document.querySelector(`#${divId}`) as HTMLElement));
+          // this.currentNumber = 2;
+          var div = (document.querySelector(`#${divId}`) as HTMLElement);
+
         }
       }
     }
   }
   MouseUp() {
     this.mousedown = false;
+  }
+  SearchAround(ArrayofObjects, x, y, count) {
+    console.log(x)
+    console.log(y)
+    if (x > 0) {
+      if (ArrayofObjects[x - 1][y].value == 0 && ArrayofObjects[x - 1][y].hasVisited == "false") {
+        ArrayofObjects[x - 1][y].hasVisited = "true";
+        ArrayofObjects[x - 1][y].count = count + 1;
+        this.SearchAround(ArrayofObjects, x - 1, y, count + 1)
+      }
+      if (ArrayofObjects[x - 1][y].value == 2) {
+        console.log("we hit a wall, waht do we do?");
+      }
+      if (ArrayofObjects[x - 1][y].value == 10 && ArrayofObjects[x - 1][y].hasVisited == "false") {
+        console.log("we found it!!!!")
+      }
+    }
+    ////now we do x + 1
+    if (x < ArrayofObjects.length - 1) {
+      if (ArrayofObjects[x + 1][y].value == 0 && ArrayofObjects[x + 1][y].hasVisited == "false") {
+        ArrayofObjects[x + 1][y].hasVisited = "true";
+        ArrayofObjects[x + 1][y].count = count + 1;
+        this.SearchAround(ArrayofObjects, x + 1, y, count + 1)
+      }
+      if (ArrayofObjects[x + 1][y].value == 2) {
+        console.log("we hit a wall, waht do we do?")
+      }
+      if (ArrayofObjects[x + 1][y].value == 10 && ArrayofObjects[x + 1][y].hasVisited == "false") {
+        console.log("we found it!!!!")
+      }
+    }
+    ////now we do y+1
+    if (y < ArrayofObjects[x].length - 1) {
+      if (ArrayofObjects[x][y + 1].value == 0 && ArrayofObjects[x][y + 1].hasVisited == "false") {
+        ArrayofObjects[x][y + 1].hasVisited = "true";
+        ArrayofObjects[x][y + 1].count = count + 1;
+        this.SearchAround(ArrayofObjects, x, y + 1, count + 1)
+      }
+      if (ArrayofObjects[x][y + 1].value == 2) {
+        console.log("we hit a wall, waht do we do?")
+      }
+      if (ArrayofObjects[x][y + 1].value == 10 && ArrayofObjects[x][y + 1].hasVisited == "false") {
+        console.log("we found it!!!!")
+      }
+
+    }
+    ////now we do y+1
+    if (y > 0) {
+      if (ArrayofObjects[x][y - 1].value == 0 && ArrayofObjects[x][y - 1].hasVisited == "false") {
+        ArrayofObjects[x][y - 1].hasVisited = "true";
+        ArrayofObjects[x][y - 1].count = count + 1;
+        this.SearchAround(ArrayofObjects, x, y - 1, count + 1)
+      }
+      if (ArrayofObjects[x][y - 1].value == 2) {
+        console.log("we hit a wall, waht do we do?")
+      }
+      if (ArrayofObjects[x][y - 1].value == 10 && ArrayofObjects[x][y - 1].hasVisited == "false") {
+        console.log("we found it!!!!")
+      }
+    }
   }
 }
 
